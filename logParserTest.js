7242da1e3e3d5e768f52a22aeec96798f509ab25
Antonio Macrì <ing.antonio.macri@gmail.com>
2017-11-01 13:34:00 +0100

    Show test results in a custom dialog window.

--- logParserTest.js
+++ logParserTest.js
@@ -26,6 +26,49 @@
 justLoad = null;
 
 
+// Internationalization
+var CANNOT_LOAD_FILE = "Cannot load \"%0\" (status: %1\).";
+var CANNOT_CREATE_UI = "Cannot create the UI dialog.";
+var NOTHING_TO_PARSE = "The editor content does not seem a valid LaTeX log. Copy the LaTeX output or a log into the editor and rerun this script.\n\nShould I parse the editor as a log now?";
+
+
+function ShowOutputDialog(markup) {
+  var xml = '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<ui version="4.0"><class>Dialog</class><widget class="QDialog" name="Dialog"><property name="windowModality"><enum>Qt::WindowModal</enum></prope' +
+    'rty><property name="geometry"><rect><x>0</x><y>0</y><width>600</width><height>500</height></rect></property><property name="minimumSize"><size><' +
+    'width>250</width><height>0</height></size></property><property name="windowTitle"><string>Results</string></property><property name="sizeGripEna' +
+    'bled"><bool>false</bool></property><property name="modal"><bool>true</bool></property><layout class="QGridLayout" name="gridLayout"><item row="0' +
+    '" column="0"><layout class="QVBoxLayout" name="verticalLayout"><property name="spacing"><number>5</number></property><property name="sizeConstra' +
+    'int"><enum>QLayout::SetMinAndMaxSize</enum></property><property name="leftMargin"><number>0</number></property><item><widget class="QTextEdit" n' +
+    'ame="textOutput"><property name="undoRedoEnabled"><bool>false</bool></property><property name="readOnly"><bool>true</bool></property></widget></' +
+    'item><item><widget class="QDialogButtonBox" name="buttonBox"><property name="sizePolicy"><sizepolicy hsizetype="Expanding" vsizetype="Fixed"><ho' +
+    'rstretch>0</horstretch><verstretch>0</verstretch></sizepolicy></property><property name="orientation"><enum>Qt::Horizontal</enum></property><pro' +
+    'perty name="standardButtons"><set>QDialogButtonBox::Ok</set></property></widget></item></layout></item></layout></widget><tabstops><tabstop>butt' +
+    'onBox</tabstop></tabstops><resources/><connections><connection><sender>buttonBox</sender><signal>accepted()</signal><receiver>Dialog</receiver><' +
+    'slot>accept()</slot><hints><hint type="sourcelabel"><x>229</x><y>371</y></hint><hint type="destinationlabel"><x>157</x><y>274</y></hint></hints>' +
+    '</connection><connection><sender>buttonBox</sender><signal>rejected()</signal><receiver>Dialog</receiver><slot>reject()</slot><hints><hint type=' +
+    '"sourcelabel"><x>297</x><y>377</y></hint><hint type="destinationlabel"><x>286</x><y>274</y></hint></hints></connection></connections></ui>';
+
+  var dialog = TW.createUIFromString(xml);
+  if (!dialog) {
+    TW.critical(null, "", CANNOT_CREATE_UI);
+    return false;
+  }
+
+  var textOutput = TW.findChildWidget(dialog, "textOutput");
+  textOutput.setText(markup);
+
+  var result = dialog.exec() == 1;
+
+  try {
+    dialog.deleteLater();
+    labelOutput.deleteLater();
+  } catch (e) { }
+
+  return result;
+}
+
+
 var file = TW.readFile("logParser.js");
 if (file.status == 0) {
   eval(file.result);
@@ -188,7 +231,7 @@ if (file.status == 0) {
   html += "<table border='0' cellspacing='0' cellpadding='4'>";
   html += s;
   html += "</table></body></html>";
-  TW.result = html;
+  ShowOutputDialog(html);
 }
 else {
   TW.warning(null, "", "Cannot load script \"logParser.js\"!");
